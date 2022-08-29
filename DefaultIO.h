b4bf9398435d9b5bdadaabf800766fa3dd36f204
yoniumi2005@gmail.com <yoniumi2005@gmail.com>
2022-08-29 17:26:33 +0300

    start

---
+++ DefaultIO.h
@@ -0,0 +1,16 @@
+
+#include "string"
+#ifndef CLIENT_DEFAULTIO_H
+#define CLIENT_DEFAULTIO_H
+
+
+class DefaultIO {
+public:
+    virtual std::string read() = 0;
+
+    virtual void write(std::string) = 0;
+};
+
+
+#endif //CLIENT_DEFAULTIO_H
+#endif SERVER_DEFAULTIO_H
