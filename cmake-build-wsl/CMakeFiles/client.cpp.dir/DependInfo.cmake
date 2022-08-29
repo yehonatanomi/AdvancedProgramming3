7140ad0688ccd9e034f1f4fc2dfec7592168036e
yoniumi2005@gmail.com <yoniumi2005@gmail.com>
2022-08-29 18:07:35 +0300

    fix the io

---
+++ cmake-build-wsl/CMakeFiles/client.cpp.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/yoniP/AdvancedProgramming3/SocketIO.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/SocketIO.cpp.o"
+  "/mnt/c/yoniP/AdvancedProgramming3/StandardIO.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/StandardIO.cpp.o"
+  "/mnt/c/yoniP/AdvancedProgramming3/utils.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/utils.cpp.o"
+  "/mnt/c/yoniP/AdvancedProgramming3/x/Knn.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/x/Knn.cpp.o"
+  "/mnt/c/yoniP/AdvancedProgramming3/x/distanceN.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/x/distanceN.cpp.o"
+  "/mnt/c/yoniP/AdvancedProgramming3/x/flower.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/x/flower.cpp.o"
+  "/mnt/c/yoniP/AdvancedProgramming3/x/pointS.cpp" "/mnt/c/yoniP/AdvancedProgramming3/cmake-build-wsl/CMakeFiles/client.cpp.dir/x/pointS.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../x"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-wsl/CMakeFiles/client.cpp.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/client.cpp.dir/SocketIO.cpp.o"
+  "CMakeFiles/client.cpp.dir/StandardIO.cpp.o"
+  "CMakeFiles/client.cpp.dir/utils.cpp.o"
+  "CMakeFiles/client.cpp.dir/x/Knn.cpp.o"
+  "CMakeFiles/client.cpp.dir/x/distanceN.cpp.o"
+  "CMakeFiles/client.cpp.dir/x/flower.cpp.o"
+  "CMakeFiles/client.cpp.dir/x/pointS.cpp.o"
+  "client.cpp"
+  "client.cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
